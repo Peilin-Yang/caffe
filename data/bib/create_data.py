e6edb51870351e8c37f04b2ad7edaee0600668a3
Peilin Yang <yangpeilyn@gmail.com>
2017-02-22 22:55:07 -0500

    data bib added

--- data/bib/create_data.py
+++ data/bib/create_data.py
@@ -46,9 +46,21 @@ def gen_list(data_dir, redo=False):
                         annotation_path_from_index(dataset, index)))
         subprocess.call(['../../build/tools/get_image_size', data_dir, 'testing.txt', 'testing_name_size.txt'])
 
+def gen_data(args):
+    for dataset in ['training', 'testing']:
+        subprocess.call(['python ../../scripts/create_annoset.py \
+            --anno-type=%s --label-map-file=%s --min-dim=%d --max-dim=%d \
+            --resize-width=%d --resize-height=%d --check-label %s \
+            %s %s.txt \
+            %s/%s/%s/%s__$db examples/$dataset_name' 
+            % (args.anno_type, args.mapfile, args.min_dim, args.max_dim, 
+                args.width, args.height, args.extra_cmd, args.dir, 
+                dataset, args.dir, args.dataset_name, args.db, 
+                args.dataset_name, dataset, args.db, args.dataset_name)], shell=True)
+
 def main(args):
     gen_list(args.data_dir, args.redo_list)
-    #gen_data(args.data_dir, args.redo_list)
+    gen_data(args)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Prepare the bib data for ssd')
@@ -61,6 +73,33 @@ if __name__ == '__main__':
     parser.add_argument('--redo_data', dest='redo_data',
             help='True to regenerate the data no matter whether they already exist',
             required=False, type=bool)
+    parser.add_argument('--dataset_name', dest='dataset_name',
+            help='',
+            required=False, default='bib', type=str)
+    parser.add_argument('--mapfile', dest='mapfile',
+            help='',
+            required=False, default='labelmap.prototxt', type=str)
+    parser.add_argument('--anno_type', dest='anno_type',
+            help='',
+            required=False, default='detection', type=str)
+    parser.add_argument('--db', dest='db',
+            help='',
+            required=False, default='lmdb', type=str)
+    parser.add_argument('--min_dim', dest='min_dim',
+            help='',
+            required=False, default=0, type=int)
+    parser.add_argument('--max_dim', dest='max_dim',
+            help='',
+            required=False, default=0, type=int)
+    parser.add_argument('--width', dest='width',
+            help='',
+            required=False, default=0, type=int)
+    parser.add_argument('--height', dest='height',
+            help='',
+            required=False, default=0, type=int)
+    parser.add_argument('--extra_cmd', dest='extra_cmd',
+            help='',
+            required=False, default='--encode-type=jpg --encoded --redo', type=str)
 
     args = parser.parse_args()
 
